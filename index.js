33c69070d11d198895302de21f2a84206606b95b
prodforward <zach@prodforward.com>
2019-09-09 15:05:59 +0200

    Config updates to deploy to Heroku

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const path = require("path")
 
-require('dotenv').config();
+require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8000
+const secret = process.env.SECRET || "stay funky fresh"
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, "client", "build")))
 
-const uri = process.env.ATLAS_URI
+const uri = process.env.MONGODB_URI
 mongoose
-    .connect(uri, { useNewUrlParser: true, useCreateIndex: true })
+    .connect(uri || "mongodb://localhost:27017/blog", { useNewUrlParser: true, useCreateIndex: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
@@ -28,6 +31,10 @@ const usersRouter = require('./routes/users')
 
 app.use('/pubs', pubsRouter)
 app.use('/users', usersRouter)
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
