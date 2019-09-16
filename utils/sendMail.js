c0a67599f78ad569e6d428849066422f05cf296a
prodforward <zach@prodforward.com>
2019-09-16 15:44:39 +0200

    Added signature to message received email

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -22,7 +22,9 @@ sendMail.post('/message_received', async (req, res, next) => {
     const { firstname, email } = req.body
     const email_to = email
     const subject = 'Message received!'
-    const body = "<p>Hi " + firstname + ",</p><p>Thanks for the message! I typically reply within a couple hours. If I'm busy though, it could be closer to 24 hours.</p><p>Cheers,</p><p>Zach</p>"
+    const signature = "<p>--<br/><strong>Zach Pritchard</strong><br/><i>Product Manager</i><br/><a href='https://zacharypritchard.com' target='_blank' rel='noopener noreferrer'>zacharypritchard.com</a></p><p><a href='http://bit.ly/zp_linkedin' target='_blank' rel='noopener noreferrer'><img src='https://images2.imgbox.com/8c/28/aylklJ0P_o.png'/></a> <a href='http://bit.ly/zp_twitter' target='_blank' rel='noopener noreferrer'><img src='https://images2.imgbox.com/30/96/GIucfPtt_o.png'/></a> <a href='http://bit.ly/zp_angellist' target='_blank' rel='noopener noreferrer'><img src='https://images2.imgbox.com/0b/4f/XKFtLXgX_o.png'/></a></p>"    
+    const body = "<p>Hi " + firstname + ",</p><p>Thanks for the message! I typically reply within a couple hours. If I'm busy though, it could be closer to 24 hours.</p><p>Cheers,<br/>Zach</p>" + signature
+    
     try {
         await sendEmail(email_to, subject, body);
         res.json('Message Received successfully sent!');
