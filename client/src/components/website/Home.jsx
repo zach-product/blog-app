f3bdc4589deb7ded76f75334b6dd30c806278b8d
prodforward <zach@prodforward.com>
2019-09-18 17:24:38 +0100

    Added styling for logo on mobile

--- client/src/components/website/Home.jsx
+++ client/src/components/website/Home.jsx
@@ -48,7 +48,7 @@ export default class Home extends Component {
                     <div style={homeContainer}>
                         <div  className="col-12 pb-2 mt-5 mt-lg-0" style={mainContent}>
                             <div className="pb-4 pb-lg-5 pt-lg-0" style={logoContainer}>
-                                <img style={logoHome} src={logo} alt="logo"/>                            
+                                <img className="logo-mobile" style={logoHome} src={logo} alt="logo"/>                            
                             </div>
                             <h1 className="mb-5 mb-lg-4 text-white">Hi. I'm Zach. Nice to meet you!</h1>
                             <div className="mb-5 mb-lg-4 text-white" style={roleStyles}>
@@ -103,7 +103,7 @@ const mainContent = {
 }
 
 const logoContainer = {
-    paddingTop: "15rem"
+    paddingTop: "12rem"
 }
 
 const logoHome = {
