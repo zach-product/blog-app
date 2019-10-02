6617708ed1306636b26c78ff5e83b2d6b29dfa6b
prodforward <zach@prodforward.com>
2019-10-02 12:47:35 +1000

    Updated server.js with errorhandler and middleware

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const cors = require('cors')
+const session = require('express-session')
 const mongoose = require('mongoose')
-const path = require("path")
+const errorHandler = require('errorhandler')
+const path = require('path')
 const sendMail = require('./utils/sendMail')
 
+// Point config variables to .env file
 require('dotenv').config()
 
+
+// Configure mongoose's promise to global promise
+mongoose.promise = global.Promise
+
+// Configure isProduction
+const isProduction = process.env.NODE_ENV === 'production'
+
+// Initiate our app
 const app = express()
 const port = process.env.PORT || 8000
 
@@ -13,6 +24,11 @@ app.use('/uploads', express.static('uploads'))
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(session({ secret: 'blog-app', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }))
+
+if(!isProduction) {
+    app.use(errorHandler())
+}
 
 const uri = process.env.MONGOLAB_YELLOW_URI || process.env.MONGODB_URI
 mongoose
@@ -27,6 +43,7 @@ db.once('open', () => {
     console.log('MongoDB successfully connected!')
 })
 
+// Models & routes
 const pubsRouter = require('./routes/pubs')
 const usersRouter = require('./routes/users')
 const imageRouter = require('./routes/image')
@@ -35,6 +52,31 @@ app.use('/pubs', pubsRouter)
 app.use('/users', usersRouter)
 app.use('/api/mail', sendMail)
 app.use('/image', imageRouter)
+
+// Error handlers & middlewares
+if(!isProduction){
+    app.use((err, req, res) => {
+        res.status(err.status || 500)
+
+        res.json({
+            errors: {
+                message: err.message,
+                error: err,
+            },
+        })
+    })
+}
+
+app.use((err, req, res) => {
+    res.status(err.status || 500);
+  
+    res.json({
+      errors: {
+        message: err.message,
+        error: {},
+      },
+    })
+  })
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"))
