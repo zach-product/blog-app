502206f1a2f468d84a0f8299e2288de4f27d42b2
prodforward <zach@prodforward.com>
2019-10-02 12:59:10 +1000

    Added methods to UserSchema

--- models/user.model.js
+++ models/user.model.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken') // Add jsonwebtoken dependency
 
 const Schema = mongoose.Schema
 
-const userSchema = new Schema(
+const UserSchema = new Schema(
     {
         firstname: {
             type: String,
@@ -30,6 +30,36 @@ const userSchema = new Schema(
     },
     { timestamps: true })
 
-const User = mongoose.model('User', userSchema)
+UserSchema.methods.setPassword = (password) => {
+    this.salt = crypto.randomBytes(16).toString('hex')
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex')
+}
+
+UserSchema.methods.validatePassword = (password) => {
+    const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex')
+    return this.hash === hash
+}
+
+UserSchema.methods.generateJWT = () => {
+    const today = new Date()
+    const expirationDate = new Date(today)
+    expirationDate.setDate(today.getDate() + 60)
+
+    return jwt.sign({
+        email: this.email,
+        id: this._id,
+        exp: parseInt(expirationDate.getTime() / 1000, 10),
+    }, 'secret')
+}
+
+UserSchema.methods.toAuthJSON = () => {
+    return {
+        _id: this._id,
+        email: this.email,
+        token: this.generateJWT(),
+    }
+}
+
+const User = mongoose.model('User', UserSchema)
 
 module.exports = User
