399057f06eab989fca379b79d07d45797635e88d
prodforward <zach@prodforward.com>
2019-10-04 12:22:27 +1000

    Added dependencies and updated server.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express')
+const cp = require('cookie-parser')
+const bp = require('body-parser')
 const cors = require('cors')
-const mongoose = require('mongoose')
-const path = require("path")
-const sendMail = require('./utils/sendMail')
+const path = require('path')
 
 require('dotenv').config()
 
-const app = express()
-const port = process.env.PORT || 8000
+const mongoose = require('mongoose')
+const passport = require('passport')
+const sendMail = require('./utils/sendMail')
 
-app.use('/uploads', express.static('uploads'))
-app.use(cors())
-app.use(express.json())
-app.use(express.static(path.join(__dirname, "client", "build")))
+const app = express()
+
+const port = process.env.PORT || 8000
 
 const uri = process.env.MONGOLAB_YELLOW_URI || process.env.MONGODB_URI
 mongoose
@@ -25,6 +25,26 @@ const db = mongoose.connection
 
 db.once('open', () => {
     console.log('MongoDB successfully connected!')
+})
+
+app.use(passport.initialize())
+
+require('./passport-config')(passport)
+
+app.use(cp())
+app.use(bp.urlencoded({extended: false}))
+app.use(bp.json())
+app.use(cors())
+app.use(express.json())
+app.use('/uploads', express.static('uploads'))
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+app.use((req, res, next) => {
+    if (req.body) log.info(req.body)
+    if (req.params) log.info(req.params)
+    if (req.query) log.inf(req.query)
+    log.info(`Received a ${req.method} request from ${req.ip} for ${req.url}`)
+    next()
 })
 
 const pubsRouter = require('./routes/pubs')
@@ -40,6 +60,7 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"))
 })
 
-app.listen(port, () => {
+app.listen(port, err => {
+    if (err) console.log(err)
     console.log(`Server running on port ${port}`)
 })
